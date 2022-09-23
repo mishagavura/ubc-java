1d0ceb3de8c4f456d385324b688d87642f5fd4d6
mishagavura <1mgavura@gmail.com>
2022-09-23 11:10:41 -0700

    added lab1

---
+++ lab1/Q2.java
@@ -0,0 +1,9 @@
+public class Q2 {
+    public static void main(String[] args) {
+        int width = 7;
+        int height = 9;
+        System.out.println("Perimeter: " + (width * 2 + height * 2));
+        System.out.println("Area: " + (width * height));
+
+    }
+}
---
+++ lab1/Q3.java
@@ -0,0 +1,8 @@
+public class Q3 {
+    public static void main(String[] args) {
+        int t = 12;
+        final double g = 9.81;
+        System.out.println("Distance after 12 seconds: " + ((g * Math.pow(t, 2)) / 2));
+    }
+
+}
